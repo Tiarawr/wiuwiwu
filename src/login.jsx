9eb68fcd98cd25d0a8a42df3bb67339cfcfbb68e
Tiarawr <yumomik@gmail.com>
2025-05-11 03:32:37 +0700

    update file login and app

--- src/login.jsx
+++ src/login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
       className="w-[1440px] h-[1024px] relative bg-white overflow-hidden"
     >
       {/* Heading */}
-      <div className="absolute left-[507px] top-[162px]">
+      <div className="absolute left-[527px] top-[162px]">
         <span className="text-black text-3xl font-medium font-poppins">
           Welcome To{" "}
         </span>
@@ -32,12 +32,12 @@ export default function Login() {
       </div>
 
       {/* Subheading */}
-      <div className="absolute left-[534px] top-[217px] text-black text-base font-medium font-poppins w-96 h-7">
+      <div className="absolute left-[554px] top-[217px] text-black text-base font-medium font-poppins w-96 h-7">
         Enter your Credentials to access your account
       </div>
 
       {/* Email */}
-      <label className="absolute left-[420px] top-[294px] text-black text-xl font-medium font-poppins">
+      <label className="absolute left-[440px] top-[294px] text-black text-xl font-medium font-poppins">
         Email address
       </label>
       <input
@@ -45,11 +45,11 @@ export default function Login() {
         placeholder="Enter your email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="absolute top-[324px] left-[420px] w-[600px] h-14 bg-white rounded-[20px] border border-stone-300 px-4 text-base font-medium text-zinc-500"
+        className="absolute top-[324px] left-[440px] w-[600px] h-14 bg-white rounded-[20px] border border-stone-300 px-4 text-base font-medium text-zinc-500"
       />
 
       {/* Password */}
-      <label className="absolute left-[420px] top-[404px] text-black text-xl font-medium font-poppins">
+      <label className="absolute left-[440px] top-[404px] text-black text-xl font-medium font-poppins">
         Password
       </label>
       <input
@@ -57,42 +57,42 @@ export default function Login() {
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="absolute top-[434px] left-[420px] w-[600px] h-14 bg-white rounded-[20px] border border-stone-300 px-4 text-base font-medium text-zinc-500"
+        className="absolute top-[434px] left-[440px] w-[600px] h-14 bg-white rounded-[20px] border border-stone-300 px-4 text-base font-medium text-zinc-500"
       />
 
       {/* Forgot password */}
-      <div className="absolute left-[881px] top-[407px] text-blue-900 text-base font-medium font-poppins cursor-pointer">
+      <div className="absolute left-[901px] top-[407px] text-blue-900 text-base font-medium font-poppins cursor-pointer">
         forgot password
       </div>
 
       {/* Login Button */}
       <button
         type="submit"
-        className="absolute top-[549px] left-[420px] w-[600px] h-14 bg-[#B56868] rounded-[20px] border border-stone-300 text-white text-xl font-bold font-poppins"
+        className="absolute top-[549px] left-[440px] w-[600px] h-14 bg-[#B56868] rounded-[20px] border border-stone-300 text-white text-xl font-bold font-poppins"
       >
         Login
       </button>
 
       {/* Divider */}
-      <div className="absolute left-[420px] top-[678px] w-[600px] h-0 outline-2 outline-offset-[-1px] outline-neutral-300" />
-      <div className="absolute left-[711px] top-[666px] text-black text-sm font-medium font-poppins">
+      <div className="absolute left-[440px] top-[678px] w-[600px] h-0 outline-2 outline-offset-[-1px] outline-neutral-300" />
+      <div className="absolute left-[731px] top-[666px] text-black text-sm font-medium font-poppins">
         Or
       </div>
 
       {/* Google Sign In */}
-      <div className="absolute left-[420px] top-[728px] w-64 h-14 bg-white rounded-[20px] border border-stone-300" />
-      <div className="absolute left-[483px] top-[746px] text-black text-lg font-medium font-poppins">
+      <div className="absolute left-[440px] top-[728px] w-64 h-14 bg-white rounded-[20px] border border-stone-300" />
+      <div className="absolute left-[503px] top-[746px] text-black text-lg font-medium font-poppins">
         Sign in with Google
       </div>
 
       {/* Apple Sign In */}
-      <div className="absolute left-[755px] top-[728px] w-64 h-14 bg-white rounded-[20px] border border-stone-300" />
-      <div className="absolute left-[824px] top-[746px] text-black text-lg font-medium font-poppins">
+      <div className="absolute left-[775px] top-[728px] w-64 h-14 bg-white rounded-[20px] border border-stone-300" />
+      <div className="absolute left-[844px] top-[746px] text-black text-lg font-medium font-poppins">
         Sign in with Apple
       </div>
 
       {/* Bottom Text */}
-      <div className="absolute left-[556px] top-[838px] w-80 h-6">
+      <div className="absolute left-[576px] top-[838px] w-80 h-6">
         <span className="text-black text-xl font-medium font-poppins">
           Don’t have an account?{" "}
         </span>
